68d5aec8e26ef5a7f6ea1325c0728b1850c8ba16
Rex Lau <woaini_lqy@qq.com>
2025-08-14 02:45:41 +0800

    更新ollama prompt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ollama import chat
 
 stream = chat(
     model='deepseek-r1',
-    messages=[{'role': 'user', 'content': '为什么天这么蓝'}],
+    messages=[{'role': 'user', 'content': '为什么天这么蓝？'}],
     stream=True,
 )
 
